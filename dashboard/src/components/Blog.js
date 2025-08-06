340665ae51e087db7181b8b7bcfcf1517de4c4f1
jsackitey1 <sackiteyjoseph44@gmail.com>
2025-08-05 23:11:11 -0700

    android apk for testing

--- dashboard/src/components/Blog.js
+++ dashboard/src/components/Blog.js
@@ -25,7 +25,7 @@ const BlogCard = styled(Card)(({ theme }) => ({
 
 const Blog = () => {
   const handleDownloadApp = () => {
-    window.open('https://expo.dev/accounts/sackjo02/projects/agromesh-mobile/builds/7bd85e1e-f3eb-48aa-8253-bb4e84cfba41', '_blank');
+    window.open('https://expo.dev/accounts/sackjo02/projects/agromesh-mobile/builds/4eff0d15-268f-4702-8c10-ef84895f0696', '_blank');
   };
 
   return (
